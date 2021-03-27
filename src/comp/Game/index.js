8021173fd793af5a3f8161f540284ce08f88e42c
Levsha1337 <dimentor.2000@mail.ru>
2021-03-28 00:57:32 +0300

    already done

---
+++ src/comp/Game/index.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import SeaBattle from './sea-battle';
+import './index.scss';
+
+import SpriteMiss from '../../res/sprite_miss.png';
+import SpriteCell from '../../res/sprite_cell.png';
+import SpriteHit from '../../res/sprite_hit.png';
+import SpriteKill from '../../res/sprite_kill.png';
+
+const styles = [
+    { backgroundImage: `url(${SpriteCell})` },
+    { backgroundImage: `url(${SpriteMiss})` },
+    { backgroundImage: `url(${SpriteHit })` },
+    { backgroundImage: `url(${SpriteKill})` }
+];
+
+export default class Game extends Component {
+    constructor(props) {
+        super(props);
+
+        this.seaBattle = new SeaBattle();
+
+        this.state = {
+            gameField: [...Array(10)].map(() => Array(10).fill(-1)),
+            shots: 0,
+            shipsLeft: 0
+        };
+    }
+
+    componentDidMount() {
+        this.restart();
+    }
+
+    restart() {
+        this.seaBattle.fillGameField();
+        this.setState({
+            gameField: [...Array(10)].map(() => Array(10).fill(-1)),
+            shots: 0,
+            shipsLeft: 10
+        });
+    }
+    
+    cellClicked(eventX, eventY) {
+        // there we have already opened cell
+        if (this.state.gameField[eventY][eventX] !== -1) return;
+        
+        let left = this.state.shipsLeft;
+        
+        // if we have zero ships alive user cant open more cells
+        if (left === 0) return;
+
+        // unlink array elements from state
+        let newField = [...this.state.gameField];
+        newField[eventY] = [...newField[eventY]];
+
+        const shot = this.seaBattle.openCell(eventX, eventY);
+
+        if (shot.result === 2) { // if we killed the ship
+            // unlink more array elements from state
+            newField = newField.map(row => [...row]);
+            const decks = shot.ship.decks;
+
+            // we marks all decks as killed instead of hitted
+            decks.forEach(deck => {
+                newField[deck.y][deck.x] = shot.result;
+            });
+            left--;
+            
+            // generate area bounds with no ships near the killed one
+            const fromTo = SeaBattle.getFromTo(
+                shot.ship.pos.x,
+                shot.ship.pos.y,
+                shot.ship.decks.length,
+                shot.ship.pos.isVertical
+            );
+
+            // filling ships-free area
+            for (let y = fromTo.fromY; y <= fromTo.toY; y++) {
+                for (let x = fromTo.fromX; x <= fromTo.toX; x++) {
+                    if (newField[y][x] === -1) newField[y][x] = 0;
+                }
+            }
+        } else { // else we have hit or miss
+            newField[eventY][eventX] = shot.result;
+        }
+        
+        this.setState({
+            gameField: newField,
+            shots: this.state.shots + 1,
+            shipsLeft: left
+        });
+    }
+
+    render() {
+        const isWin = this.state.shipsLeft === 0;
+        return ( <div id='game'>
+            <div id='texts'>
+                <div>
+                    {isWin ? 'You WIN!' : `Ships left: ${this.state.shipsLeft}`}
+                </div>
+                <div>
+                    Shots: {this.state.shots}
+                </div>
+                <button onClick={() => this.restart()}>Restart</button>
+            </div>
+            {
+                this.state.gameField.map((row, y) => <div key={y} className='row'>{
+                    row.map((cell, x) =>
+                        <div onClick={() => this.cellClicked(x, y)}
+                            key={x}
+                            style={styles[cell+1]}
+                            className='cell'>
+                            {/* {cell.ship} */}
+                        </div>
+                    )
+                }</div>)
+            }
+        </div>
+        );
+    }
+}
---
+++ src/comp/Game/sea-battle.js
@@ -0,0 +1,145 @@
+const SHIPS = [
+    {
+        decks: 4,
+        count: 1
+    }
+    ,
+    {
+        decks: 3,
+        count: 2
+    },
+    {
+        decks: 2,
+        count: 3
+    },
+    {
+        decks: 1,
+        count: 4
+    }
+];
+
+class Cell {
+    constructor(opened, ship) {
+        this.opened = opened;
+        this.ship = ship;
+    }
+}
+
+export default class SeaBattle {
+    constructor() {
+        this.ships = [];
+        this.gameField = [];
+    }
+
+    random(n) {
+        return Math.floor(Math.random() * (n + 1));
+    }
+
+    // TODO
+    openCell(x, y) {
+        const cell = this.gameField[y][x];
+        cell.opened = true;
+
+        const id = cell.ship;
+        if (id !== 0) {
+            const ship = this.ships[id - 1];
+
+            return {
+                shipsAlive: this.ships.filter(ship => ship.decksAlive > 0).length,
+                result: --ship.decksAlive === 0 ? 2 : 1,
+                ship
+            };
+        }
+
+        return {
+            shipsAlive: this.ships.filter(ship => ship.decksAlive > 0).length,
+            result: 0
+        };
+    }
+
+    fillGameField() {
+        this.clearGameField();
+
+        SHIPS.map(shipType => {
+            for (let i = 0; i < shipType.count; i++) {
+                const pos = this.generatePosition(shipType.decks);
+                this.setShip(pos, shipType);
+            }
+        });
+    }
+
+    clearGameField() {
+        this.ships = [];
+        this.gameField = [...Array(10)].map(() => Array(10).fill(new Cell(false, 0)));
+    }
+
+    generatePosition(decks) {
+        while (true) {
+            const isVertical = Boolean(this.random(1));
+            
+            let x, y;
+            if (isVertical) {
+                x = this.random(9);
+                y = this.random(10 - decks);
+            } else {
+                x = this.random(10 - decks);
+                y = this.random(9);
+            }
+
+            if (!this.validatePosition(x, y, decks, isVertical)) continue;
+            
+            return ({x, y, isVertical});
+        }
+    }
+
+    static getFromTo(x, y, decks, isVertical) {
+        // we starts from x - 1 & y - 1
+        // using Math.max() to prevent getting out of bound
+        const fromX = Math.max(0, x - 1);
+        const fromY = Math.max(0, y - 1);
+
+        // and checking for x + 1 & y + 1
+        let toX, toY;
+        if (isVertical) {
+            toX = Math.min(9, x + 1);
+            toY = Math.min(9, y + decks);
+        } else {
+            toX = Math.min(9, x + decks);
+            toY = Math.min(9, y + 1);
+        }
+
+        return {
+            fromX, fromY, toX, toY
+        };
+    }
+
+    validatePosition(x, y, decks, isVertical) {
+        const { fromX, fromY, toX, toY } = SeaBattle.getFromTo(x, y, decks, isVertical);
+
+        if (this.gameField.slice(fromY, toY + 1)
+            .filter(arr =>
+                arr.slice(fromX, toX + 1).filter(cell => cell.ship > 0).length > 0
+            ).length > 0) return false;
+
+        return true;
+    } 
+
+    setShip(pos, shipType) {
+        const id = this.ships.length + 1;
+        const ship = {
+            id,
+            decks: [],
+            decksAlive: shipType.decks,
+            pos
+        };
+
+        for (let i = 0; i < shipType.decks; i++) {
+            const x = pos.isVertical ? pos.x : pos.x + i;
+            const y = pos.isVertical ? pos.y + i : pos.y;
+            this.gameField[y][x] = new Cell(false, id);
+            ship.decks.push({x, y});
+        }
+
+        this.ships.push(ship);
+    }
+}
